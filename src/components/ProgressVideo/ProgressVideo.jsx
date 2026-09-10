49f88b199dc4989149196b931173bf99ea139a23
RasmusSchultz <rasmus-schultz@proton.me>
2026-09-10 09:43:36 +0200

    Improved variables

--- src/components/ProgressVideo/ProgressVideo.jsx
+++ src/components/ProgressVideo/ProgressVideo.jsx
@@ -2,21 +2,31 @@ import s from "./ProgressVideo.module.css";
 
 async function fetchData() {
   try {
-    const response = await fetch("https://squid-app-uaozl.ondigitalocean.app/media");
+    const response = await fetch(
+      "https://squid-app-uaozl.ondigitalocean.app/media",
+    );
     const data = await response.json();
-    return data.data.media[0]
+    return data.data;
   } catch (error) {
     console.error("Issue during video fetch:", error);
   }
 }
 
-const video = await fetchData();
+const data = await fetchData();
+const video = data.media[0];
 
 export function ProgressVideo() {
-console.log(video)
+  console.log(video);
   return (
     <div className={s.frame}>
-      <video className={s.video} src={video.url} autoPlay muted loop playsInline></video>
+      <video
+        className={s.video}
+        src={video.url}
+        autoPlay
+        muted
+        loop
+        playsInline
+      ></video>
     </div>
   );
 }
